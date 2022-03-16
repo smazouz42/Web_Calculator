7a1a1639df494566f1a56b12a9bd147d97bf86e3
smazouz42 <gbolive01@gmail.com>
2022-03-16 19:28:23 +0100

    Inital Commit (Add web calculator)

---
+++ calculator.js
@@ -0,0 +1,80 @@
+//*************************** Global variables *******************************//
+let result;
+let oldResult;
+//******************************** DOM Elements ******************************//
+const input = document.querySelector('.result');
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equal');
+const delet =  document.querySelector('.DEL');
+const scf = document.querySelector('.scf');
+//******************************** functions *******************************//
+function clickNumber(event)
+{
+    if(result !== undefined)
+    {
+        result = (result + event.target.textContent);
+        input.value = result;
+    }
+    else
+    {
+        result = event.target.textContent
+    }
+    input.value = result;
+
+}
+
+function clickOperator(event)
+{
+    if(result !== undefined)
+    {
+        op = event.target.textContent;
+        result = result + event.target.textContent;
+    }
+    else
+        result = event.target.textContent;
+    input.value = result;
+}
+/**
+ * calculate screen hook
+ */
+function calculate()
+{
+    try {
+        result = eval(result);
+        oldResult = result;
+        input.value = result;
+    } catch (error) {
+        window.alert("Syntax error");
+        remove();
+        oldResult = 0;
+    }
+}
+//******************************** Event hooks *******************************//
+equal.addEventListener('click',calculate);
+function remove()
+{
+    result = undefined;
+    input.value = 0;
+}
+delet.addEventListener('click',remove);
+
+scf.addEventListener('click',function()
+{
+    input.value = oldResult;
+});
+
+input.addEventListener("keyup", function(event) {
+    if(event.code === "Enter")
+        calculate();
+    else
+        result = input.value;
+  });
+
+numbers.forEach(function (number){
+    number.addEventListener('click', clickNumber);
+})
+
+operators.forEach(function(operator){
+    operator.addEventListener('click', clickOperator);
+})
